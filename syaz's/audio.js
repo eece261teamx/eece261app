ec9ceb3bce7a6fe3d5cee0428f097ede1c6473ca
Muhammad Syazwan Ahmad <muhammad.s.ahmad@vanderbilt.edu>
2012-10-11 20:16:13 -0500

    guitar tuner
    guitar tuner finished,
    ukulele and bass not done yet

---
+++ syaz's/audio.js
@@ -0,0 +1,23 @@
+var audios = new Array();
+i=0;
+AddNote("Standard-E");
+AddNote("Standard-A");
+AddNote("Standard-D");
+AddNote("Standard-G");
+AddNote("Standard-B");
+AddNote("Standard-highE");
+AddNote("DropD-D");
+
+function AddNote(name) {
+    var audioElement = document.createElement('audio');
+	audioElement.setAttribute("preload", "auto");
+	audioElement.autobuffer = true;
+
+	var source1 = document.createElement('source');
+	source1.type= 'audio/mpeg';
+	source1.src= 'tunes/'+name+'.mp3';
+	audioElement.appendChild(source1);
+	audios[i] = audioElement;
+	audios[i].load();
+	i++	
+}
